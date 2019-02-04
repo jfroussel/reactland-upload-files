7d7364cf309f6078b5f3cee9ddae159af1cc3074
jeff <jeff@jeffHome.lan>
2019-02-04 10:26:36 +0100

    add progress bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import UploadImg from '../upload.svg'
 import './styles.scss'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
 
 const style = {
   progress: {
@@ -11,32 +14,77 @@ const style = {
 }
 
 export default class UploadFiles extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      files: '',
+      label: '',
+      value: '45'
+    }
+
+    this.fileInput = React.createRef()
+    this.handleChange = this.handleChange.bind(this)
+   
+  }
+
+  handleChange(event) {
+    event.preventDefault()
+    this.setState({ files: this.fileInput.current.files })
+  }
+
+  bytesToSize(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    if (bytes === 0) return 'n/a'
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
+    if (i === 0) return `${bytes} ${sizes[i]})`
+    return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
+  }
+
+  Reset() {
+    if (!this.state.files) {
+      return this.setState({ files: '' })
+    }
+  }
+
   static propTypes = {
     //text: PropTypes.string
   }
 
   render() {
-
+    const {value} = this.state
+    
+    console.log(this.state.files.length)
     const Upload = () => {
       return (
-        <div className="row">
-          <div className="col-1">
-            <div style={{}}>
-              <i className=" far fa-file-code pt-3" style={{ color: 'rgb(208, 213, 218)', fontSize: 30 }}  ></i>
-            </div>
-          </div>
-          <div className="container col-11">
-            <div>My Uploade.jpg</div>
-            <span>25 Mo</span>
-            <div>
-              <div className="progress">
-                <div className="progress-bar" role="progressbar" styles={style.progres} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+        [...this.state.files].map((e, i) => {
+          return (
+            <div className="row" key={i}>
+              <div className="col-1">
+                <div style={{}}>
+                  <i className=" far fa-file-code" style={{ color: 'rgb(208, 213, 218)', fontSize: 30, paddingTop:'1.8rem' }}  ></i>
+                </div>
+              </div>
+              <div className="container col-11">
+                <div className="font-weight-light" style={{fontSize:13}}>{e.name}</div>
+                <span className="font-weight-light" style={{fontSize:10}}>{this.bytesToSize(e.size)} {`(${e.type})`}</span>
+                <div>
+                  <div>
+                    <ProgressBar animated now={45} />
+                  </div>
+                </div>
+                <div className="font-weight-light" style={{fontSize:10}}>40% Done </div>
+                <hr />
               </div>
             </div>
-            <div>40% Done </div>
-            <hr />
-          </div>
-        </div>
+          )
+
+        })
+      )
+    }
+
+    const RenderResetButton = () => {
+      return (
+        <button className="btn btn-sm btn-info">reset</button>
       )
     }
 
@@ -45,23 +93,33 @@ export default class UploadFiles extends Component {
       <div className="container pt-5">
         <form>
           <div className="row pt-3">
-            <div className="col-4 text-center">
+            <div className="col-4 text-center mr-2" style={{ border: 'solid 1px rgb(226, 227, 228)' }}>
               <h3 className="text-center text-uppercase font-weight-lighter">File Upload</h3>
               <div className="fileUpload">
                 <img className="svg" src={UploadImg}></img>
                 <div className="font-weight-lighter pb-3">Drag Files to Upload</div>
                 <div className="wrapper">
-                  <input type="file" multiple id="file" />
-                  <label htmlFor="file">choose a file</label>
+                  <input
+                    type="file"
+                    multiple
+                    id="file"
+                    ref={this.fileInput}
+                    onChange={this.handleChange}
+                  />
+                  <label htmlFor="file">{this.state.files ? 'Upload': 'choose a file'}</label>
                 </div>
               </div>
             </div>
-            <div className="col-8">
-              <div className="font-weight-lighter">UPLOADING ...</div>
-              <div className="container">
+            <div className="col-7" style={{ border: 'solid 1px rgb(226, 227, 228)'}}>
+              <div className="font-weight-lighter">UPLOADING ... {`${this.state.files.length} file(s)` }</div>
+              <div className="text-right">
+                {
+                  this.state.files ? <RenderResetButton /> : null
+                }
+
+              </div>
+              <div className="container" >
                 <div>
-                  <Upload />
-                  <Upload />
                   <Upload />
                 </div>
               </div>
